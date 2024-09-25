2dbb21d48586d452b73a63c8ad1c6c5743a3be79
Тимур Абдалов <TimurAbdalov24@gmail.com>
2024-09-25 22:22:38 +0300

    task3

--- Roman_Sakutin/task3/task3.go
+++ Roman_Sakutin/task3/task3.go
@@ -7,10 +7,16 @@ func main() {
 	var (
 		name string
 		age  int
+		znak string
+		car  string
 	)
 	fmt.Scan(&name)
 	fmt.Println("Введите ваш возраст")
 	fmt.Scan(&age)
-	fmt.Printf("Ваше имя %v. Ваш возраст %v ", name, age)
+	fmt.Println("Введите ваш знак зодиака")
+	fmt.Scan(&znak)
+	fmt.Println("Любимая машина?")
+	fmt.Scan(&car)
+	fmt.Printf("Ваше имя %v. Ваш возраст %v. Знак задиака %v. Любимая машина %v. ", name, age, znak, car)
 
 }
