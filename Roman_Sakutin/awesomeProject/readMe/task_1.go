985dc5686d727215180d0d1791709169516588de
Тимур Абдалов <TimurAbdalov24@gmail.com>
2024-10-27 15:15:08 +0300

    task_2

--- Roman_Sakutin/awesomeProject/readMe/task_1.go
+++ Roman_Sakutin/awesomeProject/readMe/task_1.go
@@ -1,1 +1,18 @@
-package readMe
+package main
+
+import "fmt"
+
+func main() {
+
+	var a string
+	var b int
+
+	fmt.Print("Введите сообщение: ")
+	fmt.Scanln(&a)
+	fmt.Print("Введите количество повторов: ")
+	fmt.Scanln(&b)
+
+	for i := 0; i < b; i++ {
+		fmt.Println(a)
+	}
+}
--- Roman_Sakutin/awesomeProject/readMe/task_2.go
+++ Roman_Sakutin/awesomeProject/readMe/task_2.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a string
+
+	// Цикл выполняется, пока не введено слово "exit"
+	for {
+		fmt.Print("Введите команду (или 'exit' для выхода): ")
+		fmt.Scanln(&a)
+
+		if a == "exit" {
+			fmt.Println("Выход из программы.")
+			break
+		}
+
+		fmt.Println("Вы ввели:", a)
+	}
+}
